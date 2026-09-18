33062c5fcaf1aa2f6368091a10532f9e43715a2b
Robert Lönnqvist <robert@robertlonnqvist.com>
2026-09-18 16:11:25 +0200

    Rollback to rpm fusion

--- build_files/install.sh
+++ build_files/install.sh
@@ -6,56 +6,29 @@ set -e
 rm -f /opt
 mkdir -p /opt/brave.com
 
-# MATCH UBLUE: Disable the problematic history archive repo right away
-# This prevents dnf distro-sync from breaking on older Mesa references later.
-dnf config-manager disable updates-archive
-
-# Add Negativo17 Multimedia Repository and elevate priority
-dnf config-manager addrepo --from-repofile="https://negativo17.org/repos/fedora-multimedia.repo"
-dnf config-manager setopt fedora-multimedia.priority=90
+# Install rpm fusion for media drivers
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-44.noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-44.noarch.rpm
 
 # Remove stuff we dont need (Some stuff taken from ublue/main)
 dnf -y remove \
-    firefox \
-    firefox-langpacks \
-    gnome-software \
-    fedora-third-party
+  firefox \
+  firefox-langpacks \
+  gnome-software \
+  fedora-third-party
 
 # Install the things we need like brave and support for brew
 dnf -y install \
-    brave-origin \
-    zsh \
-    gcc \
-    gcc-c++ \
-    glibc-devel \
-    libxcrypt-compat \
-    binutils \
-    make
+  brave-origin \
+  zsh \
+  gcc \
+  gcc-c++ \
+  glibc-devel \
+  libxcrypt-compat \
+  binutils \
+  make \
+  intel-media-driver
 
-# =========================================================================
-# UNIVERSAL BLUE GRAPHICS SYNC ARRAY (For Intel & AMD)
-# =========================================================================
-# Swaps stock graphics with unlocked Negativo17 files seamlessly.
-# =========================================================================
-OVERRIDES=(
-    "intel-gmmlib"
-    "intel-mediasdk"
-    "intel-vpl-gpu-rt"
-    "libheif"
-    "libva"
-    "libva-intel-media-driver"
-    "mesa-dri-drivers"
-    "mesa-filesystem"
-    "mesa-libEGL"
-    "mesa-libGL"
-    "mesa-libgbm"
-    "mesa-va-drivers"
-    "mesa-vulkan-drivers"
-)
-
-dnf distro-sync --skip-unavailable -y --repo='fedora-multimedia' "${OVERRIDES[@]}"
-dnf versionlock add "${OVERRIDES[@]}"
+dnf remove -y rpmfusion-free-release rpmfusion-nonfree-release
 
 dnf clean all
-
-rm -f /etc/yum.repos.d/rpmfusion-*.repo
